77827c60877ae68f6cfc6b168ef61733bd204990
teoxuhui <23015489@myrp.edu.sg>
2024-11-28 15:49:23 +0800

    hi

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,25 +1,18 @@
 import React from "react";
 
 export default function Habit({ habit, onToggleHabit, onDeleteHabit }) {
-  return (
-    <div
-
-    >
-      <input
-        type="checkbox"
-        checked={habit.completed}
-        onChange={() => onToggleHabit(habit.id)} 
-      />
-      <span
-       
-      >
-        {habit.name}
-      </span>
-      <button
-        onClick={() => onDeleteHabit(habit.id)} 
-      >
-        Delete
-      </button>
-    </div>
-  );
-}
+    return (
+      <div>
+        <input
+          type="checkbox"
+          checked={habit.completed} // Checked based on habit's state
+          onChange={() => onToggleHabit(habit.id)} // Toggle the completion
+        />
+        <span style={{ textDecoration: habit.completed ? "line-through" : "none" }}>
+          {habit.name}
+        </span>
+        <button onClick={() => onDeleteHabit(habit.id)}>Delete</button>
+      </div>
+    );
+  }
+  
